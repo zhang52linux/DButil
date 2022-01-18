4d227507f853aae3a633f06d150b750e5dd01840
today <z52linux@gmail.com>
2022-01-18 14:40:10 +0800

    update a little

--- aiomongo/common/async_mongo.py
+++ aiomongo/common/async_mongo.py
@@ -1,12 +1,15 @@
 # -*- coding:utf-8 -*-
 from loguru import logger
 import asyncio
+import pymongo
 import motor.motor_asyncio
 from pymongo.operations import DeleteOne, UpdateOne
 from pymongo.database import Database
 from pymongo.collection import Collection
 from pymongo.errors import BulkWriteError
 from pymongo.results import BulkWriteResult
+from typing import Sequence
+from .safe_function import retry_if_exception
 
 
 class MongoGetter:
@@ -21,14 +24,12 @@ class MongoGetter:
         self.total_size = total_size
         self.fetch_count = 0
         self.buffer = []
-        self.cursor = cursor if cursor else self.client.find(self.body, self.return_fields)
+        self.cursor = cursor if cursor else self.client.find(self.body, self.return_fields).batch_size(page_size)
 
     async def get_data(self):
         if self.total_size is None:
-            if self.body:
-                self.total_size = await self.client.count_documents(self.body)
-            else:
-                self.total_size = await self.client.estimated_document_count()
+            self.total_size = await self.client.count_documents(self.body) if self.body else await self.client.estimated_document_count()
+
         if self.fetch_count >= self.total_size:
             raise StopAsyncIteration
         async for document in self.cursor:
@@ -38,11 +39,12 @@ class MongoGetter:
                 break
 
     def __aiter__(self):
+        '''Return an async iterator object'''
         return self
 
     async def __anext__(self, retry=1):
         try:
-            await self.get_data()  # 调用一次就会往buffer中插入10000条记录
+            await self.get_data()
             finished_rate = self.fetch_count / self.total_size if self.total_size else 1
             logger.info("fetch count {} from {}, total count {}, finished {:.5f}%".format(self.fetch_count, self.collection, self.total_size, finished_rate * 100))
             result, self.buffer = self.buffer, []
@@ -70,11 +72,9 @@ class MongoWriter:
     async def write(self, documents, raise_error=True):
         # nInserted 不存在则插入， $set用来更新字段的
         if not documents:
-            return {"writeErrors": [], "writeConcernErrors": [], "nInserted": 0, "nUpserted": 0, "nMatched": 0,
-                    "nModified": 0, "nRemoved": 0, "upserted": []}
-        # upsert 意思是不存在则插入: 特殊的更新,如果没有找到符合条件的更新文档条件,就会以这个条件和更新文档为基础创建一个新的文档
-        # 存在则正常更新，优点: 解决了多并发下先查询是否存在在决定是插入还是更新导致的竞态问题
+            return {"writeErrors": [], "writeConcernErrors": [], "nInserted": 0, "nUpserted": 0, "nMatched": 0, "nModified": 0, "nRemoved": 0, "upserted": []}
         documents = [UpdateOne({'_id': each["_id"]}, {"$set": each}, upsert=True) for each in documents]
+
         self.total_count += len(documents)
         for i in range(self.retry):
             try:
@@ -122,6 +122,33 @@ class AsyncMongo:
 
     # ------------------------------------------------------------------------ #
 
+    async def create_index(self, coll_name: str, keys: Sequence, sort_type=pymongo.ASCENDING, unique=False):
+        """Creates an index on this collection.
+
+        Args:
+            coll_name ([type]): collection name.
+            keys (Sequence): The key that creates the index(Single or compound).
+            sort_type (int, optional): Type of index created. Defaults to pymongo.ASCENDING.
+                `pymongo.ASCENDING`, `pymongo.DESCENDING`, `pymongo.GEO2D`, `pymongo.GEOHAYSTACK`, `pymongo.GEOSPHERE`, `pymongo.HASHED`, `pymongo.TEXT`
+            unique (bool, optional): creates a uniqueness constraint on the index. Defaults to True.
+
+        """
+        coll = self.get_collection(coll_name)
+        _keys = [(key, sort_type) for key in keys] if isinstance(keys, (list, tuple)) else [(keys, sort_type)]
+
+        return await coll.create_index(_keys, unique=unique, background=True)
+
+    async def get_index_info(self, coll_name: str):
+        """ Get collection index information. """
+        return await self.get_collection(coll_name).index_information()
+
+    async def get_count(self, coll_name: str, filter: dict = {}):
+        """ Get the number of collection documents. """
+        collect = self.get_collection(coll_name)
+        return await collect.count_documents(filter) if filter else await collect.estimated_document_count()
+
+    # ------------------------------------ CRUD ------------------------------------ #
+
     def getter(self, collection, body=None, return_fields=None, page_size=1000, retry=5):
         return MongoGetter(self.get_collection(collection), collection, body, return_fields, page_size, retry=retry)
 
@@ -149,17 +176,28 @@ class AsyncMongo:
         projection = dict.fromkeys(return_fields, 1) if return_fields else None
         return await self.get_collection(coll_name).find_one(filter, projection) or {}
 
-    async def fetch_all(self, coll_name: str, filter: dict = {}, return_fields: list = None, retry:int = 3, raise_error: bool = True):
+    @retry_if_exception(BaseException)
+    async def fetch_all(self, coll_name: str, filter: dict = {}, return_fields: list = None, batch_size: int = 10000, retry: int = 3, raise_error: bool = True):
+        '''
+        cursor:
+            - 游标是一种能从包括多条数据记录的结果集中每次提取一条记录的机制
+            - 游标充当指针的作用
+            - 游标是一种临时的数据库对象
+            - 游标由结果集和结果集中指向特定记录的游标位置组成
+        cursor用途:
+            - 游标的一个常见用途就是保存查询结果
+            - 如果处理过程需要重复使用一个记录集，那么创建一次游标而重复使用若干次，比重复查询数据库要快的多
+        batch_size:
+            - 限制每批响应中要返回的文档数。每个批次都需要往返服务器(限制游标最多有batch_size个数据)
+            - 比如说: select语句对应的数据集有10000条数据, batch_size设置为100, 则要往返服务器100次去获取数据
+            - 每批次最多返回16mb数据(return 4-16MB of results per batch)
+            - 如果字段多且内容复杂, 可能当游标中的数据只有1000条的时候, 数据量已经达到了16mb, 那么实际游标中只有1000条数据,而不会是设置的batch_size个
+        limit:
+            - 限制此游标返回的结果数, 只从游标中取多少数据
+        '''
         data = list()
         projection = dict.fromkeys(return_fields, 1) if return_fields else None
-        cursor = self.get_collection(coll_name).find(filter, projection) or {}
-        for i in range(retry):
-            try:
-                async for document in cursor:
-                    data.append(document)
-            except Exception as e:
-                if i + 1 >= retry and raise_error:
-                    raise
-            else:
-                break
+        cursor = self.get_collection(coll_name).find(filter, projection).sort('_id', pymongo.ASCENDING).batch_size(batch_size)
+        async for document in cursor:
+            data.append(document)
         return data
--- aiomongo/update_fileds.py
+++ aiomongo/update_fileds.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import asyncio
+from asyncio.log import logger
 from common.async_mongo import AsyncMongo
+
 
 class BaseMongo:
     uri_dic = dict(
@@ -20,18 +22,31 @@ class BaseMongoSpider(BaseMongo):
     async def writer_data(self, coll, data):
         writer = self.mongo.writer(coll)
         return await writer.write(data)
-    
+
     async def reader_data(self, coll_name, filter):
         return self.mongo.getter(collection=coll_name, body=filter)
 
+    async def find_all(self, coll_name, filter):
+        return await self.mongo.fetch_all(coll_name=coll_name, filter=filter)
+
 
 async def updateFileds():
-    coll = "dossen_project_data"
-    filter = {"dataType": "roomPiceAndRisk", "rid": "0759017", "appCode": "ctrip"}
+    coll = "dossen_project_bussiness_data"
+    filter = {"dataType": "bussiness", "appCode": "meituan", "dossenId": "0772076"}
     mongo_writer = BaseMongoSpider()
+    import time
+    start_time = time.time()
     getter = await mongo_writer.reader_data(coll_name=coll, filter=filter)
     async for docs in getter:
+        # logger.info("-------------------------------------")
         await mongo_writer.writer_data(coll=coll, data=docs)
+    end_time = time.time()
+    print(end_time - start_time)
+
+    start_time = time.time()
+    result = await mongo_writer.find_all(coll_name=coll, filter=filter)
+    end_time = time.time()
+    print(end_time - start_time)
 
 
 if __name__ == '__main__':
